0f111e017f092afea458ede4fb7840a9c4f50052
Ayden <Aydenalv6282@gmail.com>
2025-05-01 16:41:47 -0400

    Added pygame visualization, fixed up console.

---
+++ src/ai.py
@@ -0,0 +1,236 @@
+import torch
+from torch import nn
+import numpy as np
+import random
+import tictactoe as TTT
+
+class TicTacMaster(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # 9*9*2+9*2+9 = 189, for the input board + winner matrix + next board
+        # 9*9 = 81, for the output, choosing a square to place on
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(189, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 81),
+            nn.Softmax(dim=-1) # Necessary because we want the bot's credence for the best move
+        )
+
+    def forward(self, x):
+        # No need to use flatten since "x" is already 1D.
+        return self.linear_relu_stack(x)
+
+
+# Takes a game and a list of credences, chooses the highest credence that is a VALID move.
+# Returns the number of the square move. A number from 0-81.
+def max_valid_move(game, credence_tensor: torch.Tensor) -> int:
+    max_index = None
+    max_val = -1
+    for square in range(81):
+        if game.valid_moves[square] == 1:
+            if credence_tensor[square] > max_val:
+                max_index = square
+                max_val = credence_tensor[square]
+    return max_index
+
+# Turn a number of a square into the relevant tuples used to make a move.
+def square_to_tuple(square: int) -> tuple[tuple[int, int], tuple[int, int]]:
+    row = square // 27
+    col = square % 27 // 9
+    row_m = square % 9 // 3
+    col_m = square % 9 % 3
+    return (row, col), (row_m, col_m)
+
+#Random player for testing purposes
+def tic_tac_rand(board):
+    rt = torch.rand(81)
+    move_in, move_out = square_to_tuple(max_valid_move(board, rt))
+    return move_in, move_out
+
+def bot_vs_random(model, tests, display):
+    bot_wins = 0
+    rand_wins = 0
+
+    model.eval()
+    for i in range(tests):
+        game = TTT.SuperTicTacToe()
+        current_player = TTT.Cell.X  # Start with Player X
+
+        bot_character = random.choice([TTT.Cell.X, TTT.Cell.O])
+        rand_character = TTT.Cell.X if bot_character == TTT.Cell.O else TTT.Cell.O
+        while True:
+            if current_player == rand_character:
+                move = tic_tac_rand(game)
+            else:
+                with torch.no_grad():
+                    if bot_character == TTT.Cell.X:
+                        model_output = model.forward(torch.Tensor(game.X_flatten_board()))
+                        move = square_to_tuple(max_valid_move(game, model_output))
+                    else:
+                        model_output = model.forward(torch.Tensor(game.O_flatten_board()))
+                        move = square_to_tuple(max_valid_move(game, model_output))
+            game.make_move(move[0], move[1], current_player)
+            # Switch players
+            current_player = TTT.Cell.O if current_player == TTT.Cell.X else TTT.Cell.X
+            winner = game.check_winner()
+            if winner is not TTT.Cell.B:
+                if winner == bot_character:
+                    if display:
+                        print("The bot won!")
+                    bot_wins += 1
+                elif winner == rand_character:
+                    if display:
+                        print("The random player won!")
+                    rand_wins += 1
+                if display:
+                    print("=" * 25)
+                    print("=" * 25)
+                    game.display_board()
+                break
+        if i % (tests/1000) == 0:
+            print(f"{round(i / tests * 100, 3)}%")
+    print(f"Bot wins: {round(bot_wins / tests * 100, 3)}%")
+    print(f"Random wins: {round(rand_wins / tests * 100, 3)}%")
+
+def train(model: TicTacMaster, epochs: int, learning_rate: float, bot_portion: float, random_portion: float,
+          batch_size: int):
+    import src.tictactoe as TTT
+    # Set up the hyperparameters
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    for i in range(epochs): # Here, epochs * batch_size is the total number of games the bot is trained on.
+        cur_move = 0
+        cur_move_b = 0
+        board_history = []
+        move_history = []
+        valid_move_history = []
+
+        board_history_b = []
+        move_history_b = []
+        valid_move_history_b = []
+
+        tensor_correction = []
+        tensor_correction_b = []
+        for b in range(batch_size):
+            against_bot = random.random() < bot_portion
+
+            '''
+            Game move logic here:
+            =======
+            '''
+            game = TTT.SuperTicTacToe()
+            current_player = TTT.Cell.X  # Start with Player X
+
+            bot_character = random.choice([TTT.Cell.X, TTT.Cell.O])
+
+            with torch.no_grad(): # Reduces computations
+                model.eval()
+                while True:
+                    if current_player == bot_character:
+                        is_choice_random = random.random() < random_portion
+                        if bot_character == TTT.Cell.X:
+                            if not is_choice_random:
+                                model_output = model.forward(torch.Tensor(game.X_flatten_board()))
+                            else:
+                                model_output = torch.rand(81)
+                            board_history.append(game.X_flatten_board())
+                        else:
+                            if not is_choice_random:
+                                model_output = model.forward(torch.Tensor(game.O_flatten_board()))
+                            else:
+                                model_output = torch.rand(81)
+                            board_history.append(game.O_flatten_board())
+                        move_made = max_valid_move(game, model_output)
+                        outer_index, inner_index = square_to_tuple(move_made)
+                        move_history.append(move_made)
+                        valid_move_history.append(game.get_valid_moves())
+                    else:
+                        if not against_bot:
+                            outer_index, inner_index = tic_tac_rand(game)
+                        else:
+                            is_choice_random = random.random() < random_portion
+                            if bot_character == TTT.Cell.X:
+                                if not is_choice_random:
+                                    model_output = model.forward(torch.Tensor(game.O_flatten_board()))
+                                else:
+                                    model_output = torch.rand(81)
+                                board_history_b.append(game.O_flatten_board())
+                            else:
+                                if not is_choice_random:
+                                    model_output = model.forward(torch.Tensor(game.X_flatten_board()))
+                                else:
+                                    model_output = torch.rand(81)
+                                board_history_b.append(game.X_flatten_board())
+                            move_made = max_valid_move(game, model_output)
+                            outer_index, inner_index = square_to_tuple(move_made)
+                            move_history_b.append(move_made)
+                            valid_move_history_b.append(game.get_valid_moves())
+
+                    game.make_move(outer_index, inner_index, current_player)
+                    # Switch players
+                    current_player = TTT.Cell.O if current_player == TTT.Cell.X else TTT.Cell.X
+                    winner = game.check_winner()
+                    if winner != TTT.Cell.B:
+                        break
+            '''
+            =======
+            '''
+
+            #TRAINING
+            winner = game.check_winner()
+
+            if winner == bot_character:
+                # If the bot won, reinforce its actions by telling it to increase its confidence in the moves it made.
+                for m in move_history[cur_move:]:
+                    new_tensor = np.zeros(81)
+                    new_tensor[m] = 1
+                    tensor_correction.append(new_tensor)
+                # If bot playing against itself, penalize it for losing too.
+                if against_bot:
+                    for m in range(cur_move_b, len(move_history_b)):
+                        new_tensor = valid_move_history_b[m]
+                        new_tensor[move_history_b[m]] = 0
+                        tensor_correction_b.append(new_tensor)
+            elif winner == TTT.Cell.X or winner == TTT.Cell.O:
+                # This will set the corrections to a 1 for each valid move OTHER than the one made for a losing bot.
+                for m in range(cur_move, len(move_history)):
+                    new_tensor = valid_move_history[m]
+                    new_tensor[move_history[m]] = 0
+                    tensor_correction.append(new_tensor)
+                # Since the bot also won against itself, it should be rewarded.
+                if against_bot:
+                    for m in move_history_b[cur_move_b:]:
+                        new_tensor = np.zeros(81)
+                        new_tensor[m] = 1
+                        tensor_correction_b.append(new_tensor)
+            else: # In the case of a draw, both sides are punished
+                for m in range(cur_move, len(move_history)):
+                    new_tensor = valid_move_history[m]
+                    new_tensor[move_history[m]] = 0
+                    tensor_correction.append(new_tensor)
+                if against_bot:
+                    for m in range(cur_move_b, len(move_history_b)):
+                        new_tensor = valid_move_history_b[m]
+                        new_tensor[move_history_b[m]] = 0
+                        tensor_correction_b.append(new_tensor)
+            cur_move = len(move_history)
+            cur_move_b = len(move_history_b)
+        # Combine the history of all moves and board states
+        tensor_correction += tensor_correction_b
+        board_history += board_history_b
+        board_history = torch.Tensor(np.array(board_history))
+        corrections = torch.Tensor(np.array(tensor_correction))
+        model.train()
+        # Calculate error between credence and correct move
+        outputs = model(board_history)
+        #print(outputs, corrections)
+        loss = loss_fn(outputs, corrections)
+        # Backprop
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        if i % (epochs/1000) == 0 or epochs < 1000:
+            print(f"{round(i/epochs*100,3)}%")
+    torch.save(model.state_dict(), "files/model_weights.pth")
---
+++ src/ui.py
@@ -0,0 +1,166 @@
+import src.tictactoe as TTT
+import src.ai as TTM
+
+import pygame as pg
+
+import random
+import torch
+
+
+tiles = [(pg.Rect(25,23,45,46),(0,0,0,0)),(pg.Rect(86,23,45,46),(0,0,0,1)),(pg.Rect(146,23,45,46),(0,0,0,2)),
+         (pg.Rect(223,23,45,46),(0,1,0,0)),(pg.Rect(284,23,45,46),(0,1,0,1)),(pg.Rect(344,23,45,46),(0,1,0,2)),
+         (pg.Rect(422,23,45,46),(0,2,0,0)),(pg.Rect(483,23,45,46),(0,2,0,1)),(pg.Rect(544,23,45,46),(0,2,0,2)),
+         (pg.Rect(25,84,45,46),(0,0,1,0)),(pg.Rect(86,84,45,46),(0,0,1,1)),(pg.Rect(146,84,45,46),(0,0,1,2)),
+         (pg.Rect(223,84,45,46),(0,1,1,0)),(pg.Rect(284,84,45,46),(0,1,1,1)),(pg.Rect(344,84,45,46),(0,1,1,2)),
+         (pg.Rect(422,84,45,46),(0,2,1,0)),(pg.Rect(483,84,45,46),(0,2,1,1)),(pg.Rect(544,84,45,46),(0,2,1,2)),
+         (pg.Rect(25,145,45,46),(0,0,2,0)),(pg.Rect(86,145,45,46),(0,0,2,1)),(pg.Rect(146,145,45,46),(0,0,2,2)),
+         (pg.Rect(223,145,45,46),(0,1,2,0)),(pg.Rect(284,145,45,46),(0,1,2,1)),(pg.Rect(344,145,45,46),(0,1,2,2)),
+         (pg.Rect(422,145,45,46),(0,2,2,0)),(pg.Rect(483,145,45,46),(0,2,2,1)),(pg.Rect(544,145,45,46),(0,2,2,2)),
+         (pg.Rect(25,221,45,46),(1,0,0,0)),(pg.Rect(86,221,45,46),(1,0,0,1)),(pg.Rect(146,221,45,46),(1,0,0,2)),
+         (pg.Rect(223,221,45,46),(1,1,0,0)),(pg.Rect(284,221,45,46),(1,1,0,1)),(pg.Rect(344,221,45,46),(1,1,0,2)),
+         (pg.Rect(422,221,45,46),(1,2,0,0)),(pg.Rect(483,221,45,46),(1,2,0,1)),(pg.Rect(544,221,45,46),(1,2,0,2)),
+         (pg.Rect(25,282,45,46),(1,0,1,0)),(pg.Rect(86,282,45,46),(1,0,1,1)),(pg.Rect(146,282,45,46),(1,0,1,2)),
+         (pg.Rect(223,282,45,46),(1,1,1,0)),(pg.Rect(284,282,45,46),(1,1,1,1)),(pg.Rect(344,282,45,46),(1,1,1,2)),
+         (pg.Rect(422,282,45,46),(1,2,1,0)),(pg.Rect(483,282,45,46),(1,2,1,1)),(pg.Rect(544,282,45,46),(1,2,1,2)),
+         (pg.Rect(25,343,45,46),(1,0,2,0)),(pg.Rect(86,343,45,46),(1,0,2,1)),(pg.Rect(146,343,45,46),(1,0,2,2)),
+         (pg.Rect(223,343,45,46),(1,1,2,0)),(pg.Rect(284,343,45,46),(1,1,2,1)),(pg.Rect(344,343,45,46),(1,1,2,2)),
+         (pg.Rect(422,343,45,46),(1,2,2,0)),(pg.Rect(483,343,45,46),(1,2,2,1)),(pg.Rect(544,343,45,46),(1,2,2,2)),
+         (pg.Rect(25,421,45,46),(2,0,0,0)),(pg.Rect(86,421,45,46),(2,0,0,1)),(pg.Rect(146,421,45,46),(2,0,0,2)),
+         (pg.Rect(223,421,45,46),(2,1,0,0)),(pg.Rect(284,421,45,46),(2,1,0,1)),(pg.Rect(344,421,45,46),(2,1,0,2)),
+         (pg.Rect(422,421,45,46),(2,2,0,0)),(pg.Rect(483,421,45,46),(2,2,0,1)),(pg.Rect(544,421,45,46),(2,2,0,2)),
+         (pg.Rect(25,482,45,46),(2,0,1,0)),(pg.Rect(86,482,45,46),(2,0,1,1)),(pg.Rect(146,482,45,46),(2,0,1,2)),
+         (pg.Rect(223,482,45,46),(2,1,1,0)),(pg.Rect(284,482,45,46),(2,1,1,1)),(pg.Rect(344,482,45,46),(2,1,1,2)),
+         (pg.Rect(422,482,45,46),(2,2,1,0)),(pg.Rect(483,482,45,46),(2,2,1,1)),(pg.Rect(544,482,45,46),(2,2,1,2)),
+         (pg.Rect(25,543,45,46),(2,0,2,0)),(pg.Rect(86,543,45,46),(2,0,2,1)),(pg.Rect(146,543,45,46),(2,0,2,2)),
+         (pg.Rect(223,543,45,46),(2,1,2,0)),(pg.Rect(284,543,45,46),(2,1,2,1)),(pg.Rect(344,543,45,46),(2,1,2,2)),
+         (pg.Rect(422,543,45,46),(2,2,2,0)),(pg.Rect(483,543,45,46),(2,2,2,1)),(pg.Rect(544,543,45,46),(2,2,2,2))]
+
+
+# A function that prevents players from crashing the game with their invalid inputs.
+def ask_until_valid(prompt,req):
+    user_input = input(prompt)
+    while not req(user_input):
+        user_input = input("Invalid input. Please try again: ")
+    return user_input
+
+def guide_player(game, char):
+    if game.next_board is None:
+        print(f"Player {char}, you may go anywhere.")
+    elif game.next_board == (0,0):
+        print(f"Player {char}, you must play in the top left board.")
+    elif game.next_board == (0,1):
+        print(f"Player {char}, you must play in the top middle board.")
+    elif game.next_board == (0,2):
+        print(f"Player {char}, you must play in the top right board.")
+    elif game.next_board == (1,0):
+        print(f"Player {char}, you must play in the middle left board.")
+    elif game.next_board == (1,1):
+        print(f"Player {char}, you must play in the middle board.")
+    elif game.next_board == (1,2):
+        print(f"Player {char}, you must play in the middle right board.")
+    elif game.next_board == (2,0):
+        print(f"Player {char}, you must play in the bottom left board.")
+    elif game.next_board == (2,1):
+        print(f"Player {char}, you must play in the bottom middle board.")
+    elif game.next_board == (2,2):
+        print(f"Player {char}, you must play in the bottom right board.")
+
+def main():
+
+    print("Welcome to Super Tic Tac Toe!")
+    mode = ask_until_valid("Would you like to play or train? ",
+                           lambda x: x.lower() in ["train","play"]).lower()
+    if mode == "train":
+        batch_size = int(ask_until_valid("What batch size would you like? ",
+                                         lambda x: x.isnumeric() and int(x) > 0))
+        epochs = int(ask_until_valid("How many epochs would you like? ",
+                                         lambda x: x.isnumeric() and int(x) > 0))
+        learning_rate = float(ask_until_valid("What learning rate would you like? ",
+                                         lambda x: x.replace(".","",1).isnumeric() and float(x) > 0))
+        print("The following inputs should be a number from 0-1.")
+        random_portion = float(ask_until_valid("During training,what portion of the bot's actions should be random? ",
+                                         lambda x: x.replace(".","",1).isnumeric() and 0 <= float(x) <= 1))
+        bot_portion = float(ask_until_valid("What portion of training games should the bot play against itself? ",
+                                         lambda x: x.replace(".","",1).isnumeric() and 0 <= float(x) <= 1))
+        print("Training...")
+
+        model = TTM.TicTacMaster()
+        TTM.train(model,epochs,learning_rate,bot_portion,random_portion,batch_size)
+
+        print("Testing...")
+        TTM.bot_vs_random(model,10000,False)
+
+    game_mode = int(ask_until_valid("Would you like to:\n1. Play against another player.\n2. Play against a bot.\
+        \n3. Test a bot against random players.\n",lambda x: x in ["1","2","3"]))
+    model = TTM.TicTacMaster()
+    model.load_state_dict(torch.load('files/model_weights.pth',weights_only=False))
+    if game_mode == 3:
+        tests = int(ask_until_valid("How many test games against random players would you like? ", lambda x: x.isnumeric() and x > 0))
+        display = ask_until_valid("Would you like to display the ends of each game? ", lambda x: x.lower() in ["yes", "no"]).lower() == "yes"
+        TTM.bot_vs_random(model,tests,display)
+    else:
+        pg.init()
+        game = TTT.SuperTicTacToe()
+        screen = pg.display.set_mode((612,612))
+        clock = pg.time.Clock()
+        running = True
+
+        board = pg.transform.scale(pg.image.load("files/board.png"),(612,612))
+        board_rect = board.get_rect()
+        X = pg.transform.scale(pg.image.load("files/x.png"),(46,46))
+        O = pg.transform.scale(pg.image.load("files/o.png"),(46,46))
+
+        screen.blit(board,board_rect)
+
+        player = random.choice([TTT.Cell.X, TTT.Cell.O])
+        game = TTT.SuperTicTacToe()
+        current_player = TTT.Cell.X
+        has_won = False
+
+        while running:
+            winner = game.check_winner()
+            if not has_won and winner != TTT.Cell.B:
+                player_as_char = "X" if winner == TTT.Cell.X else "O"
+                print(f"{player_as_char} won!")
+                has_won = True
+            if game_mode == 2 and not has_won and  current_player != player:
+                bot_move = TTM.square_to_tuple(TTM.max_valid_move(game, model.forward(torch.Tensor(
+                    game.X_flatten_board() if current_player == TTT.Cell.X
+                    else game.O_flatten_board()))))
+                game.make_move(bot_move[0], bot_move[1], current_player)
+                for rec1, tile1 in tiles:
+                    if tile1 == (bot_move[0][0], bot_move[0][1], bot_move[1][0], bot_move[1][1]):
+                        screen.blit(X, rec1) if current_player == TTT.Cell.X else screen.blit(O, rec1)
+                        break
+                current_player = TTT.Cell.O if current_player == TTT.Cell.X else TTT.Cell.X
+                player_as_char = "X" if current_player == TTT.Cell.X else "O"
+                guide_player(game, player_as_char)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_r: # Reset the game by pressing r
+                        game = TTT.SuperTicTacToe()
+                        current_player = TTT.Cell.X
+                        screen.blit(board, board_rect)
+                        player = random.choice([TTT.Cell.X, TTT.Cell.O])
+                elif event.type == pg.MOUSEBUTTONDOWN and not has_won:
+                    pos = pg.mouse.get_pos()
+                    for rec,tile in tiles:
+                        if rec.collidepoint(pos) and (game.next_board is None or (tile[0], tile[1]) == game.next_board):
+                            if game.boards[tile[0], tile[1]].board[tile[2], tile[3]] == TTT.Cell.B:
+                                screen.blit(X,rec) if current_player == TTT.Cell.X else screen.blit(O,rec)
+                                game.make_move((tile[0], tile[1]), (tile[2], tile[3]), current_player)
+                                current_player = TTT.Cell.O if current_player == TTT.Cell.X else TTT.Cell.X
+                                if game_mode != 2:
+                                    player_as_char = "X" if current_player == TTT.Cell.X else "O"
+                                    guide_player(game, player_as_char)
+                                break
+            pg.display.flip()
+            clock.tick(60)/1000
+        pg.quit()
+    input() # Prevent console window from closing before you can read results
+
+
+if __name__ == "__main__":
+    main()
